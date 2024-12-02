10603acd627d44e07beac5b04bfaa94e25d66db4
Kyriakos Nicola <knicola@users.noreply.github.com>
2024-12-01 19:04:54 -0500

    extract http handler

---
+++ src/handler.ts
@@ -0,0 +1,75 @@
+/* eslint-disable no-console */
+import { Readable } from 'stream'
+import http from 'http'
+import { parseURL } from './placeholder/parser'
+import { config } from './config'
+import { generateSVGDocument, loadFonts, convertSVGToImage } from './placeholder/placeholder'
+
+// Utility function to send responses
+function write (
+    res: http.ServerResponse,
+    body: string | Buffer | Readable,
+    code = 200,
+    contentType = 'text/plain',
+): void {
+    res.writeHead(code, { 'Content-Type': contentType })
+    if (body instanceof Readable) {
+        body.pipe(res)
+    } else {
+        res.end(body)
+    }
+}
+
+// Preload fonts
+loadFonts(config.fonts.values() as any)
+
+// The main request handler
+export function requestHandler (req: http.IncomingMessage, res: http.ServerResponse): void {
+    if (req.method !== 'GET') {
+        return write(res, 'Method Not Allowed', 405)
+    }
+
+    if (! req.url || req.url === '/') {
+        return write(res, 'OK')
+    }
+
+    if (req.url === '/favicon.ico') {
+        return write(res, 'No favicon', 404)
+    }
+
+    const options = parseURL(req.url)
+    if (! options) {
+        return write(res, 'Invalid URL', 400)
+    }
+
+    const svg = generateSVGDocument(options)
+    const { format } = options
+
+    if (format === 'svg') {
+        res.writeHead(200, {
+            'Content-Type': 'image/svg+xml',
+            'Cache-Control': 'public, max-age=31536000, immutable',
+        })
+        res.end(svg)
+        return
+    }
+
+    convertSVGToImage(svg, format as any)
+        .on('data', () => {
+            if (! res.headersSent) {
+                res.writeHead(200, {
+                    'Content-Type': `image/${format}`,
+                    'Transfer-Encoding': 'chunked',
+                    'Cache-Control': 'public, max-age=31536000, immutable',
+                })
+            }
+        })
+        .on('error', (err) => {
+            console.error('Error rendering image', { err, options })
+            if (! res.headersSent) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' })
+                res.end('Internal Server Error')
+            }
+        })
+        .pipe(res)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,79 +1,12 @@
 /* eslint-disable no-console */
 import http from 'http'
-import { Readable } from 'stream'
-import { parseURL } from './placeholder/parser'
+import { AddressInfo } from 'net'
+import { requestHandler } from './handler'
 import { config } from './config'
-import { generateSVGDocument, loadFonts, convertSVGToImage } from './placeholder/placeholder'
-import type { AddressInfo } from 'net'
 
-function write (
-    res: http.ServerResponse,
-    body: string | Buffer | Readable,
-    code = 200,
-    contentType = 'text/plain',
-): void {
-    res.writeHead(code, { 'Content-Type': contentType })
-    if (body instanceof Readable) {
-        body.pipe(res)
-    } else {
-        res.end(body)
-    }
-}
-
-// preload fonts
-loadFonts(config.fonts.values() as any)
-
-const server = http.createServer((req, res) => {
-    if (req.method !== 'GET') {
-        return write(res, 'Method Not Allowed', 405)
-    }
-
-    if (! req.url || req.url === '/') {
-        return write(res, 'OK')
-    }
-
-    if (req.url === '/favicon.ico') {
-        return write(res, 'No favicon', 404)
-    }
-
-    const options = parseURL(req.url)
-    if (! options) {
-        return write(res, 'Invalid URL', 400)
-    }
-
-    const svg = generateSVGDocument(options)
-
-    const { format } = options
-
-    if (format === 'svg') {
-        res.writeHead(200, {
-            'Content-Type': 'image/svg+xml',
-            'Cache-Control': 'public, max-age=31536000, immutable',
-        })
-        return res.end(svg)
-    }
-
-    convertSVGToImage(svg, format as any)
-        .on('data', () => {
-            if (! res.headersSent) {
-                res.writeHead(200, {
-                    'Content-Type': `image/${format}`,
-                    'Transfer-Encoding': 'chunked',
-                    'Cache-Control': 'public, max-age=31536000, immutable',
-                })
-            }
-        })
-        .on('error', (err) => {
-            console.error('Error rendering image', { err, options })
-            if (! res.headersSent) {
-                res.writeHead(500, { 'Content-Type': 'text/plain' })
-                res.end('Internal Server Error')
-            }
-        })
-        .pipe(res)
-})
+const server = http.createServer(requestHandler)
 
 server.listen(config.port, config.host, () => {
     const { address, port } = server.address() as AddressInfo
-    console.log(`Server running at http://${address}:${port}`)
+    console.info(`Server running at http://${address}:${port}`)
 })
