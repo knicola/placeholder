004f99538a246be62056ee05812d23f8aeea92cb
Kyriakos Nicola <knicola@users.noreply.github.com>
2024-12-14 12:25:59 -0500

    add graceful exit hook

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,17 @@ import http from 'node:http'
 import { AddressInfo } from 'node:net'
 import { requestHandler } from '@/handler'
 import { config } from '@/config'
+import { asyncExitHook } from 'exit-hook'
 import { logger } from '@/logger'
 
 const server = http.createServer(requestHandler)
+
+asyncExitHook(async (signal) => {
+    logger.info(`Received ${signal}, shutting down gracefully...`)
+    await new Promise((resolve, reject) => {
+        server.close((err) => err ? reject(err) : resolve(null))
+    })
+}, { wait: 500 })
 
 server.listen(config.port, config.host, () => {
     const { address, port } = server.address() as AddressInfo
